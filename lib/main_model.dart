7843b1ca293207da7241341a8a62b147ebbe6cb7
ponte1010 <t.miwa.528@nitech.jp>
2020-12-27 21:50:45 +0900

    [add]Firestoreの値をリアルタイム取得

--- lib/main_model.dart
+++ lib/main_model.dart
@@ -6,10 +6,23 @@ class MainModel extends ChangeNotifier {
   List<Todo> todoList = [];
 
   Future getTodoList() async {
-    final snapshot = await FirebaseFirestore.instance.collection('todoList').get();
+    final snapshot =
+        await FirebaseFirestore.instance.collection('todoList').get();
     final docs = snapshot.docs;
     final todoList = docs.map((doc) => Todo(doc)).toList();
     this.todoList = todoList;
     notifyListeners();
+  }
+
+  void getTodoListRealtime() {
+    final snapshots =
+        FirebaseFirestore.instance.collection('todoList').snapshots();
+    snapshots.listen((snapshot) {
+      final docs = snapshot.docs;
+      final todoList = docs.map((doc) => Todo(doc)).toList();
+      todoList.sort((a, b) => b.createdAt.compareTo(a.createdAt));
+      this.todoList = todoList;
+      notifyListeners();
+    });
   }
 }

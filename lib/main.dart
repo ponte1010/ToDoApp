f4d000ab4e1e742a95e4b754b70ef1f035dc276c
ponte1010 <t.miwa.528@nitech.jp>
2020-12-28 17:18:45 +0900

    [fix]画面遷移時のエラー修正
    "Unhandled Exception: Navigator operation requested with a context that does not include a Navigator. The context used to push or pop routes from the Navigator must be that of a widget that is a descendant of a Navigator widget."

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:todo_app/add/add_page.dart';
 import 'package:todo_app/main_model.dart';
 
 void main() async {
@@ -14,29 +15,42 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'ToDo App',
-      home: ChangeNotifierProvider<MainModel>(
-        create: (_) => MainModel()..getTodoListRealtime(),
-        child: Scaffold(
-          appBar: AppBar(
-            title: Text('ToDo App'),
-          ),
-          body: Consumer<MainModel>(builder: (context, model, child) {
-            final todoList = model.todoList;
-            return ListView(
-              children: todoList
-                  .map(
-                    (todo) => ListTile(
-                      title: Text(todo.title),
-                    ),
-                  )
-                  .toList(),
+      home: MainPage(),
+    );
+  }
+}
+
+class MainPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider<MainModel>(
+      create: (_) => MainModel()..getTodoListRealtime(),
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text('ToDo App'),
+        ),
+        body: Consumer<MainModel>(builder: (context, model, child) {
+          final todoList = model.todoList;
+          return ListView(
+            children: todoList
+                .map(
+                  (todo) => ListTile(
+                    title: Text(todo.title),
+                  ),
+                )
+                .toList(),
+          );
+        }),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () async {
+            await Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => AddPage(),
+              ),
             );
-          }),
-          floatingActionButton: FloatingActionButton(
-            onPressed: () {},
-            tooltip: 'Increment',
-            child: Icon(Icons.add),
-          ),
+          },
+          child: Icon(Icons.add),
         ),
       ),
     );
